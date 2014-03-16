18bd838306a0797edb0f3f3f48186036715538b2
James Halliday <mail@substack.net>
2014-03-16 05:36:39 -0300

    failing 3d test

--- test/triangle.js
+++ test/triangle.js
@@ -10,5 +10,6 @@ test('2d', function (t) {
 test('3d', function (t) {
     t.equal(isTriangle([ [ 3, 0, 0 ], [ 4, 1, 0 ], [ 5, 2, 0 ] ]), false);
     t.equal(isTriangle([ [ 3, 0, 0 ], [ 4, -2, 0 ], [ 5, 2, 1 ] ]), true);
+    t.equal(isTriangle([ [ 3, 0, 1 ], [ 4, -2, 1 ], [ 5, 2, 1 ] ]), true);
     t.end();
 });
