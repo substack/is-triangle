b39399e6ec089c6694c1802a9d7de315eec8a739
James Halliday <mail@substack.net>
2014-03-16 05:38:31 -0300

    extra n-dimensional tests

--- test/triangle.js
+++ test/triangle.js
@@ -13,3 +13,15 @@ test('3d', function (t) {
     t.equal(isTriangle([ [ 3, 0, 1 ], [ 4, -2, 1 ], [ 5, 2, 1 ] ]), true);
     t.end();
 });
+
+test('nd', function (t) {
+    t.equal(isTriangle([ [ 5, 3, 0, 1 ], [ 5, 4, -2, 1 ], [ 5, 5, 2, 1 ] ]), true);
+    t.equal(isTriangle([ [ 5, 3, 0, 1 ], [ 6, 4, -2, 1 ], [ 7, 5, 2, 1 ] ]), true);
+    t.equal(isTriangle([ [ 5, 3, 0, 0 ], [ 5, 4, 1, 0 ], [ 5, 5, 2, 0 ] ]), false);
+    t.equal(isTriangle([
+        [ 1, 2, 3, 4, 3, 0, 0 ],
+        [ 1, 2, 3, 4, 4, 1, 0 ],
+        [ 1, 2, 3, 4, 5, 2, 0 ]
+    ]), false);
+    t.end();
+});
