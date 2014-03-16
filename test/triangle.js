3149325fe5882bc420c3a9c6ecb7e2c499582d0a
James Halliday <mail@substack.net>
2014-03-16 05:34:40 -0300

    passing 3d test

--- test/triangle.js
+++ test/triangle.js
@@ -1,8 +1,14 @@
 var test = require('tape');
 var isTriangle = require('../');
 
-test('triangles', function (t) {
+test('2d', function (t) {
     t.equal(isTriangle([ [ 3, 0 ], [ 4, 1 ], [ 5, 2 ] ]), false);
     t.equal(isTriangle([ [ 3, 0 ], [ 4, -2 ], [ 5, 2 ] ]), true);
+    t.end();
+});
+
+test('3d', function (t) {
+    t.equal(isTriangle([ [ 3, 0, 0 ], [ 4, 1, 0 ], [ 5, 2, 0 ] ]), false);
+    t.equal(isTriangle([ [ 3, 0, 0 ], [ 4, -2, 0 ], [ 5, 2, 1 ] ]), true);
     t.end();
 });
